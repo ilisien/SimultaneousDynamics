5f042c2a53478bce2ab41f8d1fb4e8d63d9b4e85
ilisien <ian8lisien@gmail.com>
2024-07-22 14:26:03 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.51

---
+++ src/SimultaneousDynamics.jl
@@ -0,0 +1,5 @@
+module SimultaneousDynamics
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SimultaneousDynamics
+using Test
+
+@testset "SimultaneousDynamics.jl" begin
+    # Write your tests here.
+end
