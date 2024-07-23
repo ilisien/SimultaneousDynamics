c340b5459596109c746af0cbe4a23d2c99d62abf
ilisien <ian8lisien@gmail.com>
2024-07-22 23:49:41 -0400

    created types.jl

---
+++ src/Types.jl
@@ -0,0 +1,28 @@
+module Types
+
+export Pos, Node, Spring, World
+
+struct Pos
+	x::Float64
+	y::Float64
+end
+
+struct Node
+	position::Pos
+	velocity::Pos
+end
+
+struct Spring
+	connections::Tuple(Int,Int)
+	stiffness::Float64
+	relax_length::Float64
+end
+
+struct World
+	nodes::Vector{Node}
+	springs::Vector{Spring}
+	optimization_threshold::Float64
+	timestep_length::Float64
+end
+
+end
